f959c7592a5f6ab6fe71c62f25ffd28cb06cf520
MacBook Pro <macbookpro@MacBook-Pro-MacBook.local>
2020-02-10 21:46:23 +0600

    Initial commit

---
+++ src/com/company/Boss.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Boss extends Player {
+
+    public Boss(int health, int damage) {
+        super(health, damage);
+    }
+}
---
+++ src/com/company/Mental.java
@@ -0,0 +1,8 @@
+package com.company;
+
+public class Mental extends Player {
+
+    public Mental(int health, int damage) {
+        super(health, damage);
+    }
+}
